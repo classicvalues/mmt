e50541e396eb930ba98faaabf9f43125ed621935
Ryan Miller <ryan.miller-1@nasa.gov>
2020-03-10 14:23:27 -0400

    MMT-reduced-tests test branch

--- spec/features/service_drafts/forms/service_organizations_form_spec.rb
+++ spec/features/service_drafts/forms/service_organizations_form_spec.rb
@@ -9,7 +9,11 @@ describe 'Service Organizations Form', js: true do
 
   context 'when submitting the form' do
     before do
+      puts ">>>>>>>>>>>>>>>>>>>>>>>>>>"
+      puts "Before add_service_organizations: #{Time.now.strftime("%k:%M:%S")}"
       add_service_organizations
+      puts "<<<<<<<<<<<<<<<<<<<<<<<<<<"
+      puts "After add_service_organizations #{Time.now.strftime("%k:%M:%S")}"
 
       within '.nav-top' do
         click_on 'Save'
