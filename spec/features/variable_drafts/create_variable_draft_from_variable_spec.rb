8aab7560eb2c72cbc1e93cacdce7342d1e915e53
Ryan Miller <ryan.miller-1@nasa.gov>
2020-09-17 12:46:18 -0400

    MMT-20.3.3.PROD-deploy-1 Commenting out test for behavior no longer supports

--- spec/features/variable_drafts/create_variable_draft_from_variable_spec.rb
+++ spec/features/variable_drafts/create_variable_draft_from_variable_spec.rb
@@ -18,34 +18,34 @@ describe 'Create new draft from variable', reset_provider: true do
     end
   end
 
-  context 'when working with the draft of a published variable' do
-    before do
-      login
-      @native_id = 'TestVariableNativeId'
-      ingest_response, _concept_response = publish_variable_draft(name: 'Test Edit Variable Name 2', native_id: @native_id)
-    end
+  #context 'when working with the draft of a published variable' do
+  #  before do
+  #    login
+  #    @native_id = 'TestVariableNativeId'
+  #    ingest_response, _concept_response = publish_variable_draft(name: 'Test Edit Variable Name 2', native_id: @native_id)
+  #  end
 
-    context 'when trying to edit the draft name' do
-      before do
-        @variable_draft = create(:full_variable_draft, native_id: @native_id)
-        visit edit_variable_draft_path(@variable_draft)
-      end
+  #  context 'when trying to edit the draft name' do
+  #    before do
+  #      @variable_draft = create(:full_variable_draft, native_id: @native_id)
+  #      visit edit_variable_draft_path(@variable_draft)
+  #    end
 
-      it 'cannot edit the name' do
-        expect(page).to have_field('variable_draft[draft][name]', readonly: true)
-      end
-    end
+  #    it 'cannot edit the name' do
+  #      expect(page).to have_field('variable_draft[draft][name]', readonly: true)
+  #    end
+  #  end
 
-    context 'when trying to publish/update a variable draft with a different name' do
-      before do
-        @variable_draft = create(:full_variable_draft, native_id: @native_id, draft_short_name: 'An Incompatible Name')
-        visit variable_draft_path(@variable_draft)
-        click_on 'Publish Variable Draft'
-      end
+  #  context 'when trying to publish/update a variable draft with a different name' do
+  #    before do
+  #      @variable_draft = create(:full_variable_draft, native_id: @native_id, draft_short_name: 'An Incompatible Name')
+  #      visit variable_draft_path(@variable_draft)
+  #      click_on 'Publish Variable Draft'
+  #    end
 
-      it 'does not succeed' do
-        expect(page).to have_content('Variable Draft was not published successfully. Variable name [An Incompatible Name] does not match the existing variable name [Test Edit Variable Name 2]')
-      end
-    end
-  end
+  #    it 'does not succeed' do
+  #      expect(page).to have_content('Variable Draft was not published successfully. Variable name [An Incompatible Name] does not match the existing variable name [Test Edit Variable Name 2]')
+  #    end
+  #  end
+  #end
 end
