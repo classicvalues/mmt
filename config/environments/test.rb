fba04c3c7b92f13cd2145e4756761d8dd4d29eff
jdfarley <john.farley@nasa.gov>
2021-01-05 09:32:16 -0500

    Turned on CSP Reporting in SIT & UAT.  Added jQuery timing call in open_accordions

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -76,14 +76,14 @@ Rails.application.configure do
   # Feature toggle for subscriptions in MMT
   config.subscriptions_enabled = true
 
-  # Feature toggle for Content Security Policy (CSP) logging.
-  config.csplog_enabled = false
-
   # Feature toggle for UMM-T
   config.umm_t_enabled = true
 
   # Feature toggle for loss report accessibility through 'edit collection' button
   config.loss_report_enabled = true
+
+  # Feature toggle for Content Security Policy (CSP) logging.
+  config.csplog_enabled = false
 
   # Feature toggle for GKR (GCMD Keyword Recommender) recommendations
   config.gkr_enabled = true
